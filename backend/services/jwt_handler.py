2dd3d92cfcc8ee8d1d72d337620fdde82941f0ba
yashraj014 <kumaryashraj21@gmail.com>
2026-03-10 23:28:43 +0530

    improved security

--- backend/services/jwt_handler.py
+++ backend/services/jwt_handler.py
@@ -1,16 +1,17 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-
-SECRET_KEY = "supersecretkey"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+import os
+from dotenv import load_dotenv
+SECRET_KEY =os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 
 def create_access_token(data: dict):
 
     to_encode = data.copy()
 
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
 
     to_encode.update({"exp": expire})
 
