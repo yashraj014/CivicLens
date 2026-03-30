594cc7cf13473c34ff3c40743fe6b3830cc4c3ce
Aniket-8314 <aniketkumar8314@gmail.com>
2026-03-31 01:30:36 +0530

    Added Issue status column

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
 # import uuid
-from sqlalchemy import Column, Integer, String, Text, ForeignKey,Boolean,DateTime
+import enum
+from sqlalchemy import Column, Integer, String, Text, ForeignKey,Boolean,DateTime,Enum as SQLEnum
 from sqlalchemy.sql import func
 from backend.database import Base
 
@@ -13,11 +14,18 @@ class User(Base):
     is_authority=Column(Boolean,default=False,nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     
+class IssueStatus(str, enum.Enum):
+    REPORTED = "Reported"
+    IN_PROGRESS = "In Progress"
+    RESOLVED = "Resolved"
+
+    
 class Issue(Base):
     __tablename__ = "issues"
     
     id = Column(Integer,primary_key=True,index=True)
     title = Column(String,nullable=False)
     description = Column(Text,nullable=False)
+    status = Column(SQLEnum(IssueStatus), default=IssueStatus.REPORTED,nullable=False)
     is_resolved = Column(Boolean, default=False,nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
