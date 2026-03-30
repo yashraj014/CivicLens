238e6020af7f372fc5c14a8ac1cec11a47fca98a
Yash Raj <kumaryashraj21@gmail.com>
2026-03-31 01:48:38 +0530

    modified issues table

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,7 @@
 # import uuid
 import enum
-from sqlalchemy import Column, Integer, String, Text, ForeignKey,Boolean,DateTime,Enum as SQLEnum
+from sqlalchemy import Column, Integer, String,Float, Text, ForeignKey,Boolean,DateTime,Enum as SQLEnum
+# from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql import func
 from backend.database import Base
 
@@ -26,14 +27,23 @@ class IssueStatus(str, enum.Enum):
     RESOLVED = "Resolved"
 
     
-
 class Issue(Base):
     __tablename__ = "issues"
     
     id = Column(Integer,primary_key=True,index=True)
     title = Column(String,nullable=False)
     description = Column(Text,nullable=False)
+    
     status = Column(SQLEnum(IssueStatus), default=IssueStatus.REPORTED,nullable=False)
     category=Column(SQLEnum(IssueCategory),nullable=False)
-    is_resolved = Column(Boolean, default=False,nullable=False)
+    
+    latitude=Column(Float,nullable=False)
+    longitude=Column(Float,nullable=False)
+    
+    image_url = Column(String, nullable=True)
+    upvote_count = Column(Integer, default=0, nullable=False)
+    
+    reporter_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
