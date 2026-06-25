b002dd7a03a6b37d0edeb5b6d23d2119b9335fab
Aniket-8314 <aniketkumar8314@gmail.com>
2026-06-25 23:17:49 +0530

    profile-url fixed

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://civiclens-profile.vercel.app/login",
+        "https://civiclens-profile.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
